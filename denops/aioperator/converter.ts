d4c9b3405c21e4b30ab9b877004d48daba7b182c
gw31415 <gw31415@amas.dev>
2024-10-26 01:27:01 +0900

    refactor: use jsr and format

--- denops/aioperator/converter.ts
+++ denops/aioperator/converter.ts
@@ -1,50 +1,50 @@
+import { ChatPromptTemplate } from "npm:@langchain/core/prompts";
+import type { ChatOpenAI, ChatOpenAICallOptions } from "npm:@langchain/openai";
+import { JsonOutputFunctionsParser } from "npm:langchain/output_parsers";
 import { z } from "npm:zod";
 import { zodToJsonSchema } from "npm:zod-to-json-schema";
-import { ChatOpenAI, ChatOpenAICallOptions } from "npm:@langchain/openai";
-import { JsonOutputFunctionsParser } from "npm:langchain/output_parsers";
-import { ChatPromptTemplate } from "npm:@langchain/core/prompts";
 
 /**
  * Convert the source text according to the given instruction.
  */
 export async function* convert(
-  model: ChatOpenAI<ChatOpenAICallOptions>,
-  instruction: string,
-  source: string,
+	model: ChatOpenAI<ChatOpenAICallOptions>,
+	instruction: string,
+	source: string,
 ): AsyncGenerator<string> {
-  const modelParams = {
-    functions: [
-      {
-        name: "replace",
-        description: "Replace a string with another string",
-        parameters: zodToJsonSchema(
-          z.object({
-            text: z
-              .string()
-              .describe(
-                "The post-conversion string to replace the pre-conversion location.",
-              ),
-          }),
-        ),
-      },
-    ],
-    function_call: { name: "replace" },
-  };
+	const modelParams = {
+		functions: [
+			{
+				name: "replace",
+				description: "Replace a string with another string",
+				parameters: zodToJsonSchema(
+					z.object({
+						text: z
+							.string()
+							.describe(
+								"The post-conversion string to replace the pre-conversion location.",
+							),
+					}),
+				),
+			},
+		],
+		function_call: { name: "replace" },
+	};
 
-  const prompt = ChatPromptTemplate.fromTemplate(
-    "Convert text according to [Order: {instruction}]:\n {source}",
-  );
+	const prompt = ChatPromptTemplate.fromTemplate(
+		"Convert text according to [Order: {instruction}]:\n {source}",
+	);
 
-  const chain = prompt
-    .pipe(model.bind(modelParams))
-    .pipe(new JsonOutputFunctionsParser());
+	const chain = prompt
+		.pipe(model.bind(modelParams))
+		.pipe(new JsonOutputFunctionsParser());
 
-  const stream = await chain.stream({
-    instruction,
-    source,
-  });
-  for await (const chunk of stream) {
-    const { text }: { text: string } = chunk;
-    yield text;
-  }
+	const stream = await chain.stream({
+		instruction,
+		source,
+	});
+	for await (const chunk of stream) {
+		const { text }: { text: string } = chunk;
+		yield text;
+	}
 }
--- denops/aioperator/main.ts
+++ denops/aioperator/main.ts
@@ -1,40 +1,40 @@
 import { ChatOpenAI } from "npm:@langchain/openai";
+import { isString } from "jsr:@core/unknownutil";
+import type { Denops } from "jsr:@denops/std";
 import { convert } from "./converter.ts";
-import { Denops } from "https://deno.land/x/denops_std@v5.1.0/mod.ts";
-import { isString } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts#^.ts";
 
 export function main(denops: Denops) {
-  denops.dispatcher = {
-    async start(instruction, source, openai, responseWriterFuncId) {
-      if (!isString(instruction)) {
-        throw new Error("Order must be a string");
-      }
-      if (!isString(source)) {
-        throw new Error("Source must be a string");
-      }
-      if (!isString(responseWriterFuncId)) {
-        throw new Error("Response writer ID must be a string");
-      }
-      const openaiOpts = Object.assign(
-        {
-          model: "gpt-4o",
-          apiKey: Deno.env.get("OPENAI_API_KEY") ?? "",
-          temperature: 0,
-        },
-        openai,
-      );
+	denops.dispatcher = {
+		async start(instruction, source, openai, responseWriterFuncId) {
+			if (!isString(instruction)) {
+				throw new Error("Order must be a string");
+			}
+			if (!isString(source)) {
+				throw new Error("Source must be a string");
+			}
+			if (!isString(responseWriterFuncId)) {
+				throw new Error("Response writer ID must be a string");
+			}
+			const openaiOpts = Object.assign(
+				{
+					model: "gpt-4o",
+					apiKey: Deno.env.get("OPENAI_API_KEY") ?? "",
+					temperature: 0,
+				},
+				openai,
+			);
 
-      const model = new ChatOpenAI(openaiOpts);
-      const stream = convert(model, instruction, source);
+			const model = new ChatOpenAI(openaiOpts);
+			const stream = convert(model, instruction, source);
 
-      // Stream a diff as JSON patch operations
-      for await (const replacement of stream) {
-        await denops.call(
-          "denops#callback#call",
-          responseWriterFuncId,
-          replacement,
-        );
-      }
-    },
-  };
+			// Stream a diff as JSON patch operations
+			for await (const replacement of stream) {
+				await denops.call(
+					"denops#callback#call",
+					responseWriterFuncId,
+					replacement,
+				);
+			}
+		},
+	};
 }
