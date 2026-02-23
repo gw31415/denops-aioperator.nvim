fe9fefccb6a6f9a4f8c511d24299ee2a87548e67
gw31415 <24710985+gw31415@users.noreply.github.com>
2026-02-24 01:43:31 +0900

    feat: Fix unknown typings

--- denops/aioperator/main.ts
+++ denops/aioperator/main.ts
@@ -4,37 +4,42 @@ import type { Denops } from "jsr:@denops/std";
 import { convert } from "./converter.ts";
 
 export function main(denops: Denops) {
-	denops.dispatcher = {
-		async start(instruction, source, openai, responseWriterFuncId) {
-			if (!isString(instruction)) {
-				throw new Error("Order must be a string");
-			}
-			if (!isString(source)) {
-				throw new Error("Source must be a string");
-			}
-			if (!isString(responseWriterFuncId)) {
-				throw new Error("Response writer ID must be a string");
-			}
-			const openaiOpts = Object.assign(
-				{
-					model: "gpt-5-mini",
-					apiKey: Deno.env.get("OPENAI_API_KEY") ?? "",
-					temperature: 0,
-				},
-				openai,
-			);
+  denops.dispatcher = {
+    async start(
+      instruction: unknown,
+      source: unknown,
+      openai: unknown,
+      responseWriterFuncId: unknown,
+    ) {
+      if (!isString(instruction)) {
+        throw new Error("Order must be a string");
+      }
+      if (!isString(source)) {
+        throw new Error("Source must be a string");
+      }
+      if (!isString(responseWriterFuncId)) {
+        throw new Error("Response writer ID must be a string");
+      }
+      const openaiOpts = Object.assign(
+        {
+          model: "gpt-5-mini",
+          apiKey: Deno.env.get("OPENAI_API_KEY") ?? "",
+          temperature: 0,
+        },
+        openai,
+      );
 
-			const model = new ChatOpenAI(openaiOpts);
-			const stream = convert(model, instruction, source);
+      const model = new ChatOpenAI(openaiOpts);
+      const stream = convert(model, instruction, source);
 
-			// Stream a diff as JSON patch operations
-			for await (const replacement of stream) {
-				await denops.call(
-					"denops#callback#call",
-					responseWriterFuncId,
-					replacement,
-				);
-			}
-		},
-	};
+      // Stream a diff as JSON patch operations
+      for await (const replacement of stream) {
+        await denops.call(
+          "denops#callback#call",
+          responseWriterFuncId,
+          replacement,
+        );
+      }
+    },
+  };
 }
